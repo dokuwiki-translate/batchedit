705ace58b4c4dd31f0541058ef79a68902563a6e
Eduardo Mozart de Oliveira <eduardomozart182@gmail.com>
2024-11-18 22:35:46 +0100

    translation update

---
+++ lang/pt-br/lang.php
@@ -0,0 +1,75 @@
+<?php
+
+/**
+ * @license    GPL 2 (https://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Eduardo Mozart de Oliveira <eduardomozart182@gmail.com>
+ */
+$lang['encoding']              = 'utf-8';
+$lang['direction']             = 'ltr';
+$lang['menu']                  = 'BatchEdit';
+$lang['err_invreq']            = 'Solicitação inválida.';
+$lang['err_nosearch']          = 'A expressão de pesquisa não foi especificada.';
+$lang['err_invregexp']         = 'Expressão regular inválida.';
+$lang['err_emptyidx']          = 'O índice da página está vazio.';
+$lang['err_idxaccess']         = 'Não foi possível acessar o índice da página.';
+$lang['err_emptyns']           = 'Nenhuma página encontrada no namespace "{1}".';
+$lang['err_pregfailed']        = 'Falha na correspondência de expressões regulares.';
+$lang['war_nomatches']         = 'Nenhuma correspondência foi encontrada.';
+$lang['war_norights']          = 'Você não tem privilégios para editar a página {1}.';
+$lang['war_pagelock']          = 'A página {1} está bloqueada por {2}.';
+$lang['war_matchfail']         = 'Falha ao aplicar a correspondência {1}.';
+$lang['war_searchlimit']       = 'A busca foi interrompida após atingir o número máximo de correspondências.';
+$lang['war_timeout']           = 'A operação foi interrompida porque estava demorando muito.';
+$lang['war_cancelled']         = 'A operação foi interrompida a pedido do usuário.';
+$lang['war_nosummary']         = 'O resumo da edição não foi fornecido. Você quer prosseguir sem isso?';
+$lang['msg_error']             = '<b>Erro:</b> {1}';
+$lang['msg_warning']           = '<b>Aviso:</b> {1}';
+$lang['btn_preview']           = 'Pré-visualizar';
+$lang['btn_apply']             = 'Aplicar';
+$lang['btn_cancel']            = 'Cancelar';
+$lang['hnt_textsearch']        = 'AnyWiki';
+$lang['hnt_textreplace']       = 'DokuWiki';
+$lang['hnt_regexpsearch']      = '\w+(Wiki)';
+$lang['hnt_regexpreplace']     = 'Doku$1';
+$lang['hnt_advregexpsearch']   = '/\w+(Wiki)/m';
+$lang['lbl_ns']                = 'Namespace';
+$lang['lbl_search']            = 'Procurar por';
+$lang['lbl_replace']           = 'Substitua por';
+$lang['lbl_summary']           = 'Editar resumo';
+$lang['lbl_minor']             = 'Alterações mínimas';
+$lang['lbl_searchmode']        = 'Modo de pesquisa';
+$lang['lbl_searchtext']        = 'Texto simples';
+$lang['lbl_searchregexp']      = 'Expressão regular';
+$lang['lbl_matchcase']         = 'Diferencia maiúsculas de minúsculas';
+$lang['lbl_multiline']         = 'Multilinha';
+$lang['lbl_advregexp']         = 'Usar delimitadores e modificadores na expressão regular';
+$lang['lbl_matchctx']          = 'Mostrar contexto de correspondência de {1} caracteres ou {2} linhas';
+$lang['lbl_searchlimit']       = 'Parar a pesquisa depois de encontrar as primeiras {1} correspondências';
+$lang['lbl_keepmarks']         = 'Preservar correspondências marcadas na pré-visualização {1}';
+$lang['lbl_keepmarks1']        = 'Modo seguro';
+$lang['lbl_keepmarks2']        = 'Mesma correspondência';
+$lang['lbl_keepmarks3']        = 'Mesmo deslocamento';
+$lang['lbl_keepmarks4']        = 'Estou com sorte!';
+$lang['lbl_tplpatterns']       = 'Aplicar padrões de substituição de modelo de namespace';
+$lang['lbl_checksummary']      = 'Mostrar confirmação na aplicação de edições sem resumo';
+$lang['lbl_searching']         = 'Procurando...';
+$lang['lbl_applying']          = 'Aplicando...';
+$lang['sts_preview']           = 'Resultados da pesquisa: {1} em {2}';
+$lang['sts_apply']             = 'Editar resultados: {1} em {2}, {3}';
+$lang['sts_page']              = '{1} &ndash; {2}';
+$lang['sts_matches#one']       = '{1} correspondência';
+$lang['sts_matches#many']      = '{1} correspondências';
+$lang['sts_pages#one']         = '{1} página';
+$lang['sts_pages#many']        = '{1} páginas';
+$lang['sts_edits#one']         = '{1} substituição aplicada';
+$lang['sts_edits#many']        = '{1} substituições aplicadas';
+$lang['ttl_applyall']          = 'Aplicar todas as correspondências';
+$lang['ttl_applyfile']         = 'Aplicar todas as correspondências neste arquivo';
+$lang['ttl_applymatch']        = 'Aplicar esta correspondência';
+$lang['ttl_view']              = 'Ir para esta página';
+$lang['ttl_edit']              = 'Editar esta página';
+$lang['ttl_mainform']          = 'Ir para o fim';
+$lang['ttl_extoptions']        = 'Opções avançadas';
+$lang['dim_options']           = '13em';
+$lang['dim_extoptions']        = '28em';
---
+++ lang/pt-br/settings.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * @license    GPL 2 (https://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Eduardo Mozart de Oliveira <eduardomozart182@gmail.com>
+ */
+$lang['timelimit']             = 'Limite de tempo para execução de operações do plug-in (segundos, 0 - padrão PHP)';
+$lang['sessionexp']            = 'Tempo de expiração da sessão (segundos)';
